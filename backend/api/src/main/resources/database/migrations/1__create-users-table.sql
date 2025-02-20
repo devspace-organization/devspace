bcc55906b4d2457b4160658c836dd4b1b591154d
Pedro Souza <pedro.ads510@gmail.com>
2025-02-20 20:49:30 -0300

    feat: add first database migration

---
+++ backend/api/src/main/resources/database/migrations/1__create-users-table.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    github_id UUID,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    avatar_url VARCHAR(250) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
