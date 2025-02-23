3d22f3b52a753f97002bc8f4ee947702073665f7
Pedro Souza <pedro.ads510@gmail.com>
2025-02-23 15:55:05 -0300

    refactor: add username and role to users table

--- backend/api/src/main/resources/database/migrations/1__create-users-table.sql
+++ backend/api/src/main/resources/database/migrations/1__create-users-table.sql
@@ -4,9 +4,11 @@ CREATE TABLE users (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     github_id UUID,
     name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
     email VARCHAR(250) NOT NULL,
     password VARCHAR(100) NOT NULL,
     avatar_url VARCHAR(250) NOT NULL,
+    role VARCHAR(10) NOT NULL DEFAULT("User"),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
