b688c323882b72e5048d8c2e14872bace69cd09b
Pedro Souza <pedro.ads510@gmail.com>
2025-02-28 23:41:39 -0300

    feat: create user path class

---
+++ backend/api/src/main/java/com/devspace/api/models/UserPath.java
@@ -0,0 +1,33 @@
+package com.devspace.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users_paths")
+public class UserPath {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "path_id", nullable = false)
+    private Path path;
+
+    private Integer index;
+
+}
